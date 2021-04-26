0808d098b85184f455f999dc73de7b71371f17b8
Chitra29-dev <cxanandk@student.oaklandcc.edu>
2021-04-25 22:45:16 -0400

    10:45

--- Login.php
+++ Login.php
@@ -1,17 +1,41 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+
+session_start();
+
+require('./models/database.php');
+//require('./models/stock_database.php');
+
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == NULL) {
+        $action = 'login';
+    }
+}
+
+if ($action == 'logout'){
+    $_SESSION = array();
+    session_destroy();
+    echo 'logged out';
+} else if ($action == 'login') {
+    require ('./views/login.php');
+} else if ($action == 'submitLogin') {
+    $username = filter_input(INPUT_POST, 'username');
+    $password = filter_input(INPUT_POST, 'password');
+
+    $user = get_user($username);
+
+    if ($user != NULL) {
+        if (password_verify($password, $user['password_hash'])) {
+            echo "login successful";
+            $_SESSION['is_logged_in'] = true;
+            $_SESSION['username'] = $username;
+            header("Location: ./StockOrder.php");
+        } else {
+            echo "incorrect password";
+        }
+    } else {
+        // this is not a great security practice, but we don't care for now
+        echo "user not found";
+    }
+}
--- views/Login.php
+++ views/Login.php
@@ -1,17 +1,16 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<h1>WelCome to Twitter Clone</h1>
+<h2>Login</h2>
+<form action="login.php" method="post">
+    <div id="data">
+        <label>User Name</label>
+        <input type="text" name="username"><br>
+        <label>Password</label>
+        <input type="password" name="password"><br>
+        <input type="hidden" name="action" value="submitLogin"/>
+    </div>
+    <div id="buttons">
+        <label>&nbsp;</label>
+        <input type="submit" value="Login"><br>
+    </div>
+</form>
+
