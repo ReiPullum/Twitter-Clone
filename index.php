9c2d6d58f39eaff706373a5c51f34d39a1418466
Chitra29-dev <cxanandk@student.oaklandcc.edu>
2021-04-28 13:51:45 -0400

    1.51

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ require('./views/login.php');
 $action = filter_input(INPUT_POST, 'action');
 
 
-/*if ($action == NULL) {
+if ($action == NULL) {
     $action = filter_input(INPUT_GET, 'action');
     if ($action == NULL) {
         $action = 'submitLogin';
         
     }
      require('./views/Login.php');
-}*/
+}
