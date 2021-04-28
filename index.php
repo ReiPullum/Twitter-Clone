68e750b744f1db0b2816def8b685d501b25373d3
Chitra29-dev <cxanandk@student.oaklandcc.edu>
2021-04-28 18:14:16 -0400

    6.14

--- index.php
+++ index.php
@@ -3,11 +3,12 @@ require('./models/database.php');
 require('./models/twitter_database.php');
 
 $action = filter_input(INPUT_POST, 'action');
-if ($action == NULL || $action == false || $action == "") {
+if ($action == NULL) {
     $action = filter_input(INPUT_GET, 'action');
    if ($action == NULL) {
         $action = 'tweets';
     }
+    include'./views/guest.php';
 }
 
 
